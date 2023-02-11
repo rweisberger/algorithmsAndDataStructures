94a7bf9bc2e29bdbc3688eea46aced412f11db9c
Rachel Weisberger <rachel.weisberger@gmail.com>
2023-02-11 14:39:36 -0500

    building a stack and a queue

---
+++ Part21_StackaAndQueues.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    // in this case we are adding to the beginning of the list- it is like unshift
+    push(value){
+        let newNode = new Node(value);
+        
+        if(this.stack.length === 0){
+            this.first = node;
+            this.last = node;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            newNode.next = temp;
+        }
+        this.length++;
+        return this.length;
+    }
+    pop(){
+        if(this.length === 0) return null;
+
+        let removedNode = this.first;
+        this.first = this.first.next;
+        this.length--;
+        return removedNode.value;
+    
+    }
+}
